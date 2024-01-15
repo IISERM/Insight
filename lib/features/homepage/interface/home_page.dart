027f9cc6bb2aa27383741f2ecbdbf4fe5e394bac
Tanaanshu <ms22191@iisermohali.ac.in>
2024-01-15 17:51:10 +0530

    💄Added UI for HomePage

--- lib/features/homepage/interface/home_page.dart
+++ lib/features/homepage/interface/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:insight/features/homepage/interface/widgets/facilities.dart';
+import 'package:insight/features/homepage/interface/widgets/greetings.dart';
+import 'package:insight/features/homepage/interface/widgets/helpfultiles.dart';
+import 'package:insight/features/homepage/interface/widgets/notification_container.dart';
+
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +17,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GreetingWidget(),
+              NotificationsContainer(),
+              Facilities(),
+              helpfulText,
+              HelpfulTiles(),
+
+    ],
+          ),
+        ),
+      )
+      ),
+      );
   }
 
 }
+
+Text helpfulText = const Text('Helpful');
